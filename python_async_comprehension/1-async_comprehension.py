70898df81f403208c21f1eb2998bd94b06724ddf
Allix <thewitchonxanax@gmail.com>
2026-04-10 12:45:52 +0000

    feat: Implement async comprehension to collect random floats

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module for collecting random numbers via async comprehension."""
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Collect and return 10 random floats using an async comprehension."""
+    return [i async for i in async_generator()]
