5bbc9f38a7aea22316b94d1afa09fb6b044e1482
Allix <thewitchonxanax@gmail.com>
2026-04-23 12:43:55 +0000

    fix: Update docstring format in index_range function

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,7 +4,7 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Return a tuple of start and end indexes for the given page and page_size.
+    """Return a tuple of start and end indexes for the given page and page_size
 
     Page numbers are 1-indexed, so page 1 starts at index 0.
     """
